e504c42f49ebdd3636ff21b46e92fd736bf560dc
Webplus M <contact@webplusm.net>
2019-11-19 15:23:25 -0400

    trying notification

--- sw.js
+++ sw.js
@@ -54,3 +54,40 @@ self.addEventListener("fetch", evt => {
     )
 
 });
+
+self.addEventListener('push', evt => {
+    var options = {
+        body: 'Here is a notification body!',
+        icon: 'images/example.png',
+        vibrate: [100, 50, 100],
+        data: {
+            dateOfArrival: Date.now(),
+            primaryKey: 1
+        },
+        actions: [
+            {action: 'explore', title: 'Explore this new world',
+                icon: 'images/checkmark.png'},
+            {action: 'close', title: 'Close notification',
+                icon: 'images/xmark.png'},
+        ]
+    };
+
+    console.log('show notification: event:',evt);
+
+    evt.waitUntil(
+        self.registration.showNotification('test message', options)
+    )
+});
+
+self.addEventListener('notificationclick', function(e) {
+    var notification = e.notification;
+    var primaryKey = notification.data.primaryKey;
+    var action = e.action;
+
+    if (action === 'close') {
+        notification.close();
+    } else {
+        clients.openWindow('http://www.example.com');
+        notification.close();
+    }
+});
