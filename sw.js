c3e4cffff156468d4d333e64175100cd85de6f17
Webplus M <contact@webplusm.net>
2019-11-19 15:57:54 -0400

    trying notification

--- sw.js
+++ sw.js
@@ -72,7 +72,7 @@ self.addEventListener('push', evt => {
         ]
     };
 
-    console.log('show notification: event:',evt.data);
+    console.log('show notification: event:',evt.data.json());
 
     evt.waitUntil(
         self.registration.showNotification('test message', options)
