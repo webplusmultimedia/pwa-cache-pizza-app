12988647e3bdf373e204c6cc5e64bd60eaf2d79c
Webplus M <contact@webplusm.net>
2019-11-20 10:59:58 -0400

    test d'installation app

--- sw.js
+++ sw.js
@@ -56,21 +56,7 @@ self.addEventListener("fetch", evt => {
 });
 
 self.addEventListener('push', evt => {
-    var options = {
-        body: 'Here is a notification body!',
-        icon: 'images/example.png',
-        vibrate: [100, 50, 100],
-        data: {
-            dateOfArrival: Date.now(),
-            primaryKey: 1
-        },
-        actions: [
-            {action: 'explore', title: 'Explore this new world',
-                icon: 'images/checkmark.png'},
-            {action: 'close', title: 'Close notification',
-                icon: 'images/xmark.png'},
-        ]
-    };
+
     var donnees=evt.data.json();
 
     console.log('show notification: event:',donnees);
